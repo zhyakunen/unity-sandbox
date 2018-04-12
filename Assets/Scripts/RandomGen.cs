b2c31d46efa9880721b68b343f2e12b158124dbb
zhyakunen <zhyakunenj@hotmail.com>
2018-04-12 13:40:48 +0900

    it work

--- Assets/Scripts/RandomGen.cs
+++ Assets/Scripts/RandomGen.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class RandomGen : MonoBehaviour {
 
+
     public GameObject genObject;
     [Range(0,1000)]public float width, height;
     [Range(1, 10)] public float intvFrom, intvTo;
 
+    private System.Random random;
     private float wait;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		Instantiate(genObject,)
+        random = new System.Random(System.DateTime.Now.GetHashCode());
+        wait = CalcWait();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        GenObject();
 	}
 
-    float calcWait() {
-        return Random()
+    void GenObject() {
+        wait -= Time.fixedDeltaTime;
+        if (wait <= 0f)
+        {
+            Debug.Log("beep");
+            Instantiate(genObject,GenVector(), Quaternion.identity); 
+            wait += CalcWait();
+        }
+              
+    }
+
+    Vector3 GenVector() {
+        float x = transform.position.x + Mathf.Lerp(-width, width, (float)random.NextDouble());
+        float y = transform.position.y + Mathf.Lerp(-height, height, (float)random.NextDouble());
+        return new Vector3(x, y, transform.position.z);
+    }
+
+    private float CalcWait() {
+        return Mathf.Lerp(intvFrom,intvTo,(float)random.NextDouble());
     }
 }
---
+++ Assets/Scripts/SlimeBang.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeBang : MonoBehaviour {
+
+    static float forceMulti = 1f;
+    static float slimeMulti = 4f;
+    static float size = 0.1f;
+    public GameObject bangObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void bang(float s) {
+        float f = s * forceMulti;
+        for (int i = 0; i <= s*slimeMulti; i++) {
+            Vector3 offset = Random.insideUnitSphere*size;
+            Rigidbody2D r2d = Instantiate(bangObject, transform.position + offset, Quaternion.identity).GetComponent<Rigidbody2D>();
+            r2d.AddExplosionForce(f, transform.position, 2f);
+
+        }
+        Destroy(gameObject, 1f);
+    }
+}
